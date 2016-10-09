1309d8da5673a1c65899239483d1d332f382c5ab
Claudio Vallejo <hola@claudiovallejo.mx>
2016-10-09 06:46:02 -0500

    Modified config.rb: Activated livereload.

--- development/config.rb
+++ development/config.rb
@@ -1,4 +1,4 @@
-###
+##
 # Page options, layouts, aliases and proxies
 ###
 
@@ -12,3 +12,9 @@ page '/*.txt', layout: false
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Livereload
+###
+
+activate :livereload
